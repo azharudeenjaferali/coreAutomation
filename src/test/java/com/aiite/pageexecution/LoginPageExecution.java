d3503c5b9aa1c623ba31b2ff120651a5725b9d78
AzharudeenAli <aali@tracfone.com>
2023-04-15 09:12:07 +0530

    Login Page TCs added

---
+++ src/test/java/com/aiite/pageexecution/LoginPageExecution.java
@@ -0,0 +1,12 @@
+package com.aiite.pageexecution;
+
+import org.testng.annotations.Test;
+
+public class LoginPageExecution {
+	
+	@Test
+	public void testMethod() {
+		System.out.println("Hello");
+	}
+
+}
